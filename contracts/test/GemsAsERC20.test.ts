0b9e8d0827bb7c1e00227d14850dadc66a3b5e62
wighawag <wighawag@gmail.com>
2023-08-15 10:10:50 +0100

    add tests

--- contracts/test/GemsAsERC20.test.ts
+++ contracts/test/GemsAsERC20.test.ts
@@ -1,8 +1,7 @@
-import {erc20} from 'ethereum-contracts-test-suite';
+import {erc20, runtests} from 'ethereum-contracts-test-suite';
 import {deployStratagemsWithTestConfig} from './utils/stratagems-test';
 import {loadFixture} from '@nomicfoundation/hardhat-network-helpers';
 import {describe, it} from 'vitest';
-import {TestToRun} from 'ethereum-contracts-test-suite/dist/src/testsuite';
 
 type Fixture = {
 	ethereum: any; // TODO
@@ -15,29 +14,14 @@ async function setupGems(): Promise<Fixture> {
 	const setup = await loadFixture(deployStratagemsWithTestConfig);
 	return {
 		ethereum: setup.provider,
-		contractAddress: setup.Gems.address,
+		contractAddress: setup.TestTokens.address,
 		users: setup.otherAccounts,
 		userWithToken: setup.tokensBeneficiary,
 	};
 }
 
-const tests = erc20.generateTests(setupGems);
+const tests = erc20.generateTests({EIP717: true}, setupGems);
 
-function recurse(test: TestToRun) {
-	if (test.subTests) {
-		describe(test.title, function () {
-			if (test.subTests) {
-				for (const subTest of test.subTests) {
-					recurse(subTest);
-				}
-			}
-		});
-	} else {
-		it(test.title, test.test);
-	}
-}
 describe('Gems as ERC20', function () {
-	it.each(tests)(`$title`, async (test) => {
-		recurse(test);
-	});
+	runtests(tests, {describe, it});
 });
---
+++ contracts/test/StratagemsAsERC721.test.ts
@@ -0,0 +1,48 @@
+import {deployStratagemsWithTestConfig} from './utils/stratagems-test';
+import {loadFixture} from '@nomicfoundation/hardhat-network-helpers';
+import {describe, it} from 'vitest';
+import {erc721, runtests} from 'ethereum-contracts-test-suite';
+import {xyToBigIntID} from 'stratagems-common';
+
+type Fixture = {
+	ethereum: any; // TODO
+	contractAddress: string;
+	users: string[];
+	deployer: string;
+	mint(to: string): Promise<{hash: string; tokenId: string}>;
+};
+
+async function setupStratagems(): Promise<Fixture> {
+	const setup = await loadFixture(deployStratagemsWithTestConfig);
+
+	async function mint(to: string) {
+		const tokenID = xyToBigIntID(2, 3);
+		const hash = await setup.Stratagems.write.forceSimpleCells(
+			[
+				[
+					{
+						position: tokenID,
+						owner: to as `0x${string}`,
+						color: 5,
+						life: 2,
+					},
+				],
+			],
+			{account: setup.deployer}
+		);
+		return {hash, tokenId: tokenID.toString()};
+	}
+	return {
+		ethereum: setup.provider,
+		contractAddress: setup.Stratagems.address,
+		users: setup.otherAccounts,
+		deployer: setup.deployer,
+		mint,
+	};
+}
+
+const tests = erc721.generateTests(setupStratagems);
+
+describe('Stratagems as ERC721', function () {
+	runtests(tests, {describe, it});
+});
