92e8b99e083a5cbaf1fe8431bffed0f04cca6c65
wighawag <wighawag@gmail.com>
2023-10-18 17:40:01 +0100

    tweaks

--- web/src/lib/components/utilities/WipNotice.svelte
+++ web/src/lib/components/utilities/WipNotice.svelte
@@ -5,15 +5,15 @@
 <Modal cancelation={{cancelable: false}}>
 	<h3 class="text-lg font-bold">A new play-test is being worked on</h3>
 	<p class="py-4">Please come back later!</p>
-	<p>
-		And if you did not register for the play you can do so <a
+	<p class="py-4">
+		If you did not register for the play test you can do so <a
 			href="https://backdropbeta.com/stratagems"
 			target="_blank"
 			rel="noopener noreferer"
 			class="underline">here</a
 		>
 	</p>
-	<p>
+	<p class="py-4">
 		And we recommend you join our <a
 			href="https://community.etherplay.io"
 			target="_blank"
