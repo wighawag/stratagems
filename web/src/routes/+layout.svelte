41acad6c3cef4f2e2cc3e3c48f718378e8b288ad
wighawag <wighawag@gmail.com>
2023-12-06 11:43:22 +0000

    composablelabs activation

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -145,7 +145,7 @@
 <!-- </div> -->
 
 <!-- We remove the notice when force is specified or if on base network -->
-{#if !dev && !params['force'] && initialContractsInfos.chainId + '' !== '8453'}
+{#if !dev && !params['force'] && initialContractsInfos.chainId + '' !== '8453' && initialContractsInfos.name !== 'composablelabs'}
 	<WipNotice />
 {/if}
 
