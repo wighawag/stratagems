b23a544e2878c3f139d9ad90dc2b3fc445970511
wighawag <wighawag@gmail.com>
2024-03-12 11:16:44 +0000

    wip notice date for base

--- web/src/lib/ui/components/WipNotice.svelte
+++ web/src/lib/ui/components/WipNotice.svelte
@@ -1,15 +1,17 @@
-<script>
+<script lang="ts">
+	import {initialContractsInfos} from '$lib/config';
 	import Modal from '$utils/ui/modals/Modal.svelte';
+	$: date = (initialContractsInfos as any).name == 'base' ? 'Thursday 21st March' : undefined;
 </script>
 
 <Modal>
 	<div class="wrapper">
-		<h3>A new play-test is being worked on</h3>
-		<p>
-			We recommend you join our <a href="https://community.etherplay.io" rel="noopener noreferer" class="underline"
-				>Discord</a
-			> if you did not already.
-		</p>
+		{#if date}
+			<h3>Come back on {date}</h3>
+		{:else}
+			<h3>A new play-test is being worked on</h3>
+		{/if}
+
 		<div class="modal-actions">
 			<a role="button" href="https://community.etherplay.io" rel="noopener noreferer">Join Our Community</a>
 		</div>
