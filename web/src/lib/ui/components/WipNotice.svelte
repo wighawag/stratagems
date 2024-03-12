003f3dd48afb9eb11c73b5a2c119fbe21e9d0ab8
wighawag <wighawag@gmail.com>
2024-03-12 11:21:27 +0000

    more info

--- web/src/lib/ui/components/WipNotice.svelte
+++ web/src/lib/ui/components/WipNotice.svelte
@@ -8,8 +8,20 @@
 	<div class="wrapper">
 		{#if date}
 			<h3>Come back on {date}</h3>
+			<p>
+				We recommend you join our community on <a
+					href="https://community.etherplay.io"
+					rel="noopener noreferer"
+					class="underline">Discord</a
+				>. We will be giving tokens to play the game.
+			</p>
 		{:else}
 			<h3>A new play-test is being worked on</h3>
+			<p>
+				We recommend you join our <a href="https://community.etherplay.io" rel="noopener noreferer" class="underline"
+					>Discord</a
+				> if you did not already.
+			</p>
 		{/if}
 
 		<div class="modal-actions">
